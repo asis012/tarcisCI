5cbc236542033ab2e4969d47cd67f2ee9fd93b75
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-02-14 07:02:47 +0000

    Deploy asis012/tarcisCI to github.com/asis012/tarcisCI.git:gh-pages

---
+++ precache-manifest.f636300cf5c14a6802397c4e1789fd8c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65025abe5a3cbb54f87ada4707386e91",
+    "url": "/tarcisCI/index.html"
+  },
+  {
+    "revision": "9e1e86751065806ef499",
+    "url": "/tarcisCI/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "cb5e7ef43ccc8e25c490",
+    "url": "/tarcisCI/static/js/2.8d77a238.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/tarcisCI/static/js/2.8d77a238.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e1e86751065806ef499",
+    "url": "/tarcisCI/static/js/main.2d6b552e.chunk.js"
+  },
+  {
+    "revision": "26e37f9a90dbe8b4cf0a",
+    "url": "/tarcisCI/static/js/runtime-main.42417357.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tarcisCI/static/media/logo.5d5d9eef.svg"
+  }
+]);
